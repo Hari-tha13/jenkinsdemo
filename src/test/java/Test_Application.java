d4a778e47fc9b38bcc668e589aff17fe662dd415
tvsraghuram <tvsraghuram@gmail.com>
2020-12-13 11:22:27 +0000

    New test

--- src/test/java/Test_Application.java
+++ src/test/java/Test_Application.java
@@ -15,5 +15,10 @@ public class Test_Application {
     public void test3(){
         System.out.println("test3");
     }
+    @Test
+    public void test4(){
+        System.out.println("test4");
+    }
+
 
 }
